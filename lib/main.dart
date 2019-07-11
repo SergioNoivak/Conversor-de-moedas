a76f065f61e9f8800149567e27dafe689b25de60
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-07-11 09:56:21 -0300

    feito os campos de dollar, real e euro

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,128 @@ const url = "https://api.hgbrasil.com/finance?key=391b4a06";
 
 void main() async{
 
-  http.Response response = await http.get(url);
-  print(json.decode(response.body));
-  runApp(MaterialApp(home:Container()));
+ runApp(
+   MaterialApp(
+      home:Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white
+      ),
+ )
+ 
+ 
+ );
   
+}
+
+class Home extends StatefulWidget {
+@override
+_HomeState createState() => _HomeState();
+}
+class _HomeState extends State<Home> {
+
+double dollar;
+double euro;
+
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+        backgroundColor:Colors.black ,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          actions: <Widget>[
+          ],
+        ),
+
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context,snapshot){
+            switch(snapshot.connectionState){
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(child: Text("Carregando dados", style: TextStyle(color: Colors.amber,fontSize: 10))); 
+                default:
+                  if(snapshot.hasError){
+                      return Center(child: Text("Erro ao carregar dados", style: TextStyle(color: Colors.amber,fontSize: 10))); 
+                  }
+                  else{
+                      dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                      euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                      return SingleChildScrollView(
+                        padding: EdgeInsets.all(10.0),
+                        child:Column(children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Icon(Icons.monetization_on,size:150.0, color:Colors.amber),
+                              TextField(
+                                decoration: InputDecoration(
+
+                                      labelText: "Reais",
+                                      labelStyle: TextStyle(
+                                          color: Colors.amber
+                                      ),
+                                       border: OutlineInputBorder(),
+                                      prefixText: "R\$ "
+
+                                ),
+                                style: TextStyle(color: Colors.white),
+
+                              ),
+
+                              Divider(),
+                                                      TextField(
+                                decoration: InputDecoration(
+
+                                      labelText: "Dólares",
+                                      labelStyle: TextStyle(
+                                          color: Colors.amber
+                                      ),
+                                       border: OutlineInputBorder(),
+                                      prefixText: "US\$ "
+
+                                ),
+                                style: TextStyle(color: Colors.white),
+
+                              ),
+                                        Divider(),                    TextField(
+                                decoration: InputDecoration(
+
+                                      labelText: "Euros",
+                                      labelStyle: TextStyle(
+                                          color: Colors.amber
+                                      ),
+                                       border: OutlineInputBorder(),
+                                      prefixText: "euros "
+
+                                ),
+                                style: TextStyle(color: Colors.white),
+
+                              )  
+                          ],)
+
+                      ],
+                      ) 
+                      );
+                  }
+            }
+
+          } 
+        ),
+
+);
+}
+}
+
+
+
+
+Future<Map>  getData() async{
+
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
 }
